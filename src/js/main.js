01b328059a6981bafed4268ee1e0914f73b7a8b8
jivan_supe <jivan.supe@indexnine.com>
2021-09-15 14:49:59 +0530

    Fixed folder structure and added code

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+document.getElementById('content').innerHTML = '<object type="text/html" data="../../src/public/home.html" ></object>';
+
+function loadHomeContent() {
+  document.getElementById('content').innerHTML = '<object type="text/html" data="../../src/public/home.html" ></object>';
+}
+
+function loadMenuContent() {
+  document.getElementById('content').innerHTML = '<object type="text/html" data="../../src/public/menu.html" ></object>';
+}
+
+function loadContactContent() {
+  document.getElementById('content').innerHTML = '<object type="text/html" data="../../src/public/contact.html" ></object>';
+}
